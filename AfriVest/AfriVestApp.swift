28344cbb99919265f13caf7284ea9cc6c46fdbd0
Kato Drake Smith <judecolordigital@gmail.com>
2025-10-01 19:37:32 +0300

    Initial Commit

---
+++ AfriVest/AfriVestApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfriVestApp.swift
+//  AfriVest
+//
+//  Created by Kato Drake Smith on 01/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct AfriVestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AfriVest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AfriVest
+//
+//  Created by Kato Drake Smith on 01/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
